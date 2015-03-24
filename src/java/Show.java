662fc86fe690e989529849c783a5a2ee0dfbe9f2
spari_000 <spari_000@129.49.119.197>
2015-03-24 00:33:59 -0400

    working insertion to database

--- src/java/Show.java
+++ src/java/Show.java
@@ -8,20 +8,22 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 /**
  *
  * @author spari_000
  */
-@WebServlet("/Register")
-
-public class Register extends HttpServlet {
+@WebServlet("/Show")
+public class Show extends HttpServlet {
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -36,40 +38,44 @@ public class Register extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html");
 
-        String email = request.getParameter("email");
-
-        String name = request.getParameter("name");
-
-        String password = request.getParameter("password");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(email + name + password);
-        }
+        JSONArray jsonArray = new JSONArray();
 
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            try {
-                System.out.println(email + "--");
 
-                Class.forName("com.mysql.jdbc.Driver");
-                java.util.Properties sysprops = System.getProperties();
-                sysprops.put("user", "root");
-                sysprops.put("password", "pass");
-                //connect to the database
+            Class.forName("com.mysql.jdbc.Driver");
+            java.util.Properties sysprops = System.getProperties();
+            sysprops.put("user", "root");
+            sysprops.put("password", "pass");
+            //connect to the database
 
-                Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3310/school", sysprops);
-                Statement st = con.createStatement();
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3310/school", sysprops);
+            Statement st = con.createStatement();
 
-                String query = "INSERT INTO user  " + "VALUES('" + email + "','" + name + "','" + password + "'," + "'s')";
-                System.out.println(query);
+            String query = "SELECT * FROM user";
 
-                st.executeUpdate(query);
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
-                System.out.println("error");
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+
+                JSONObject employeeToAdd = new JSONObject();
+                employeeToAdd.put("email", rs.getString("email"));
+                employeeToAdd.put("name", rs.getString("name"));
+                System.out.println(rs.getString("name"));
+                employeeToAdd.put("password", rs.getString("password"));
+                jsonArray.add(employeeToAdd);
             }
+                System.out.println(jsonArray);
+            //set the content type of our response
+            response.setContentType("application/json");
+            //printout prints it to our ajax call and it shows up there as data. you can use this data in the success function.
 
-            out.println("The user is " + name + " \nThere is a world of ajax out here");
+            out.print(jsonArray);
+            out.flush();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("error");
         }
+
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
